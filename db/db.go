2f845eb825387e7262b70e9b7d2dc5b5e2a05d84
Murad <guseinovmg@gmail.com>
2021-01-06 17:48:03 +0300

    add ENV variables

--- db/db.go
+++ db/db.go
@@ -13,8 +13,13 @@ var DB *pgxpool.Pool
 
 func init() {
 	logger := log15adapter.NewLogger(log.New("module", "pgx"))
-	poolConfig, err := pgxpool.ParseConfig(fmt.Sprintf("postgresql://%s:%s@%s:%s/%s",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME")))
+	databaseUrl := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
+	poolConfig, err := pgxpool.ParseConfig(databaseUrl)
 	if err != nil {
 		log.Crit("Unable to parse DATABASE_URL", "error", err)
 		os.Exit(1)
